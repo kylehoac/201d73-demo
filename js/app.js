8af31a29b59f98ee09f4e159458d6713c8b0f700
Kyle Hoac <hoackyle@gmail.com>
2021-03-10 14:22:40 -0800

    added number guessing game function

--- js/app.js
+++ js/app.js
@@ -1,71 +1,114 @@
-'use strict'
+// 'use strict'
 
-alert('Welcome to my "about me" website. You will be put through a series of yes or no questions that will teach you a little more about me. Please press "OK" to continue');
+// alert('Welcome to my "about me" website. You will be put through a series of yes or no questions that will teach you a little more about me. Please press "OK" to continue');
 
-function guessname(){
+// function guessName() {
 
-    let name = prompt ("Is my name Kyle?").toLowerCase();
-    console.log('The user answered ' + name)
-    if (name === "yes" || name === "y"){
-        alert ("Your answer is correct!");
-    }
-    else if(name === "no" || name === "n"){
-        alert ("Your answer is incorrect");
+//     let name = prompt("Is my name Kyle?").toLowerCase();
+//     console.log('The user answered ' + name)
+//     if (name === "yes" || name === "y") {
+//         alert("Your answer is correct!");
+//     }
+//     else if (name === "no" || name === "n") {
+//         alert("Your answer is incorrect");
+//     }
+// }
+// guessName();
+
+// function guessAge() {
+
+//     let age = prompt("Am I 21 years old?").toLowerCase();
+//     console.log('The user answered ' + age)
+//     if (age === "no" || age === "n") {
+//         alert("Your answer is correct!");
+//     }
+//     else if (age === "yes" || age === "y") {
+//         alert("Your answer is incorrect");
+//     }
+// }
+// guessAge();
+
+// function guessJob() {
+
+//     let job = prompt("Did I previously worked in education?").toLowerCase();
+//     console.log('The user answered ' + job)
+//     if (job === "no" || job === "n") {
+//         alert("Your answer is correct!");
+//     }
+//     else if (job === "yes" || job === "y") {
+//         alert("Your answer is incorrect");
+//     }
+// }
+// guessJob();
+
+// function guessColor() {
+
+//     let color = prompt("My favorite color is red").toLowerCase();
+//     console.log('The user answered ' + color)
+//     if (color === "no" || color === "n") {
+//         alert("Your answer is correct!");
+//     }
+//     else if (color === "yes" || color === "y") {
+//         alert("Your answer is incorrect");
+//     }
+// }
+// guessColor();
+
+// function guessState() {
+
+//     let state = prompt("I was born in raised in Washington").toLowerCase();
+//     console.log('The user answered ' + state)
+//     if (state === "no" || state === "n") {
+//         alert("Your answer is correct!");
+//     }
+//     else if (state === "yes" || state === "y") {
+//         alert("Your answer is incorrect");
+//     }
+// }
+// guessState();
+
+// let userName = prompt("Please enter your name!")
+// alert('Welcome ' + userName + '!')
+
+function numberGuessingGame(){
+
+    let answer = 7;
+    let guesses = 4;
+    for(let i = 0; i < guesses; i++){
+        let userAnswer = prompt(`Please enter a number from 1-10`);
+        if(userAnswer == answer){
+            alert(`You got it right!`);
+            break;
+        } else if (userAnswer < 7){
+            alert(`Guess was too low!`)
+        } else if (userAnswer > 7){
+            alert(`Guess was too high!`)
+        } else {
+            alert(`Must enter a number`)
+        }
     }
 }
-guessname();
+    numberGuessingGame();
 
-function guessage(){
 
-    let age=prompt("Am I 21 years old?").toLowerCase();
-    console.log('The user answered ' + age)
-    if(age === "no" || age === "n"){
-        alert("Your answer is correct!");
-    }
-    else if(age === "yes" || age === "y"){
-        alert("Your answer is incorrect");
-    }
-}
-guessage();
 
-function guessjob(){
-    
-    let job=prompt("Did I previously worked in education?").toLowerCase();
-    console.log('The user answered ' + job)
-    if(job === "no" || job === "n"){
-        alert("Your answer is correct!");
-    }
-    else if(job === "yes" || job === "y"){
-        alert("Your answer is incorrect");
-    }
-}
-guessjob();
+// let favColor = prompt(`Guess one of my favorite colors`)
 
-function guesscolor(){
-    
-    let color=prompt("My favorite color is red").toLowerCase();
-    console.log('The user answered ' + color)
-    if(color === "no" || color === "n"){
-        alert("Your answer is correct!");
-    }
-    else if(color === "yes" || color === "y"){
-        alert("Your answer is incorrect");
-    }
-}
-guesscolor();
+// switch (favColor) {
 
-function guessstate(){
-    
-    let state=prompt("I was born in raised in Washington").toLowerCase();
-    console.log('The user answered ' + state)
-    if(state === "no" || state === "n"){
-        alert("Your answer is correct!");
-    }
-    else if(state === "yes" || state === "y"){
-        alert("Your answer is incorrect");
-    }
-}
-guessstate();
+//     case `blue`:
+//         alert(`You got one!`);
+//         break;
 
-let userName=prompt("Please enter your name!")
-    alert('Welcome ' + userName + '!')
+//     case `red`:
+//         alert(`You got one!`);
+//         break;
+
+//     case `green`:
+//         alert(`You got one!`);
+//         break;
+
+//     default:
+//         alert(`You guessed wrong!`);
+//         console.log(favcolor)
+// }
