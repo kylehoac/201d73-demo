9edce176f07da55c3ef25829bba4dd1bd0b65c5d
Kyle Hoac <hoackyle@gmail.com>
2021-03-10 16:54:51 -0800

    fixed array guessing game

--- js/app.js
+++ js/app.js
@@ -1,111 +1,109 @@
 'use strict'
 
-// alert('Welcome to my "about me" website. You will be put through a series of yes or no questions that will teach you a little more about me. Please press "OK" to continue');
+let rightans = 0 
 
-// function guessName() {
+alert('Welcome to my "about me" website. You will be put through a series of yes or no questions that will teach you a little more about me. Please press "OK" to continue');
 
-//     let name = prompt("Is my name Kyle?").toLowerCase();
-//     console.log('The user answered ' + name)
-//     if (name === "yes" || name === "y") {
-//         alert("Your answer is correct!");
-//     }
-//     else if (name === "no" || name === "n") {
-//         alert("Your answer is incorrect");
-//     }
-// }
-// guessName();
+function guessName() {
 
-// function guessAge() {
+    let name = prompt("Is my name Kyle?").toLowerCase();
+    console.log('The user answered ' + name)
+    if (name === "yes" || name === "y") {
+        alert("Your answer is correct!");
+    }
+    else if (name === "no" || name === "n") {
+        alert("Your answer is incorrect");
+    }
+}
+guessName();
 
-//     let age = prompt("Am I 21 years old?").toLowerCase();
-//     console.log('The user answered ' + age)
-//     if (age === "no" || age === "n") {
-//         alert("Your answer is correct!");
-//     }
-//     else if (age === "yes" || age === "y") {
-//         alert("Your answer is incorrect");
-//     }
-// }
-// guessAge();
+function guessAge() {
 
-// function guessJob() {
+    let age = prompt("Am I 21 years old?").toLowerCase();
+    console.log('The user answered ' + age)
+    if (age === "no" || age === "n") {
+        alert("Your answer is correct!");
+        rightans += 1
+    }
+    else if (age === "yes" || age === "y") {
+        alert("Your answer is incorrect");
+    }
+}
+guessAge();
 
-//     let job = prompt("Did I previously worked in education?").toLowerCase();
-//     console.log('The user answered ' + job)
-//     if (job === "no" || job === "n") {
-//         alert("Your answer is correct!");
-//     }
-//     else if (job === "yes" || job === "y") {
-//         alert("Your answer is incorrect");
-//     }
-// }
-// guessJob();
+function guessJob() {
 
-// function guessColor() {
+    let job = prompt("Did I previously worked in education?").toLowerCase();
+    console.log('The user answered ' + job)
+    if (job === "no" || job === "n") {
+        alert("Your answer is correct!");
+    }
+    else if (job === "yes" || job === "y") {
+        alert("Your answer is incorrect");
+    }
+}
+guessJob();
 
-//     let color = prompt("My favorite color is red").toLowerCase();
-//     console.log('The user answered ' + color)
-//     if (color === "no" || color === "n") {
-//         alert("Your answer is correct!");
-//     }
-//     else if (color === "yes" || color === "y") {
-//         alert("Your answer is incorrect");
-//     }
-// }
-// guessColor();
+function guessColor() {
 
-// function guessState() {
+    let color = prompt("My favorite color is red").toLowerCase();
+    console.log('The user answered ' + color)
+    if (color === "no" || color === "n") {
+        alert("Your answer is correct!");
+    }
+    else if (color === "yes" || color === "y") {
+        alert("Your answer is incorrect");
+    }
+}
+guessColor();
 
-//     let state = prompt("I was born in raised in Washington").toLowerCase();
-//     console.log('The user answered ' + state)
-//     if (state === "no" || state === "n") {
-//         alert("Your answer is correct!");
-//     }
-//     else if (state === "yes" || state === "y") {
-//         alert("Your answer is incorrect");
-//     }
-// }
-// guessState();
+function guessState() {
 
-// let userName = prompt("Please enter your name!")
-// alert('Welcome ' + userName + '!')
+    let state = prompt("I was born in raised in Washington").toLowerCase();
+    console.log('The user answered ' + state)
+    if (state === "no" || state === "n") {
+        alert("Your answer is correct!");
+    }
+    else if (state === "yes" || state === "y") {
+        alert("Your answer is incorrect");
+    }
+}
+guessState();
 
-// let answer = 7;
-// let guesses = 4;
-// for (let i = 0; i < guesses; i++) {
-//     let userAnswer = prompt(`Please enter a number from 1-10`);
-//     if (userAnswer == answer) {
-//         alert(`You got it right!`);
-//         break;
-//     } else if (userAnswer < 7) {
-//         alert(`Guess was too low!`)
-//     } else if (userAnswer > 7) {
-//         alert(`Guess was too high!`)
-//     } else {
-//         alert(`Must enter a number`)
-//     }
+let userName = prompt("Please enter your name!")
+alert('Welcome ' + userName + '!')
 
-// }
-// alert(`The answer is ` + answer);
+let answer = 7;
+let guesses = 4;
+for (let i = 0; i < guesses; i++) {
+    let userAnswer = prompt(`Please enter a number from 1-10`);
+    if (userAnswer == answer) {
+        alert(`You got it right!`);
+        break;
+    } else if (userAnswer < 7) {
+        alert(`Guess was too low!`)
+    } else if (userAnswer > 7) {
+        alert(`Guess was too high!`)
+    } else {
+        alert(`Must enter a number`)
+    }
 
+}
+alert(`The answer is ` + answer);
 
-// let favColor = prompt(`Guess one of my favorite colors`)
-
-// switch (favColor) {
-
-//     case `blue`:
-//         alert(`You got one!`);
-//         break;
-
-//     case `red`:
-//         alert(`You got one!`);
-//         break;
-
-//     case `green`:
-//         alert(`You got one!`);
-//         break;
-
-//     default:
-//         alert(`You guessed wrong!`);
-//         console.log(favcolor)
-// }
+let ans = [4, 6, 7];
+let guess = 6;
+for (let i = 0; i < guess; i++) {
+    let userAns = parseInt(prompt("Once again, please enter a number from 1 - 10"))
+    console.log(typeof userAns)
+    while(userAns < 1 || userAns > 10){
+        userAns=prompt(`Please enter a number between 1-10`)
+    }
+    for (let k = 0; k < ans.length; k++) {
+        if (userAns === ans[k]) {
+            alert(`You got it right! The correct answers were 4, 6, and 7!`)
+            i = 7
+            break;
+        }
+    }
+}
